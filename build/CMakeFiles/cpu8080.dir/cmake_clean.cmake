6929a13e3a1be7bf30a21e18c45c4015b24f34a9
yinxingmaimingg <yinxingmaimingg@hotmail.com>
2018-11-24 20:43:33 -0500

    first commit

---
+++ build/CMakeFiles/cpu8080.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cpu8080.dir/src/main.cpp.o"
+  "CMakeFiles/cpu8080.dir/src/Cpu.cpp.o"
+  "cpu8080.pdb"
+  "cpu8080"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cpu8080.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
